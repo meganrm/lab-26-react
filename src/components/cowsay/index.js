de21882c4cb22a62138675a42bf0cb222238c27f
meganr <meganrm@gmail.com>
2017-12-02 12:52:51 -0800

    added prop types

--- src/components/cowsay/index.js
+++ src/components/cowsay/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class CowSays extends React.Component {
   render() {
     return (<pre>{this.props.cowSays}</pre>);
   }
 }
+
+CowSays.propTypes = {
+  cowSays: PropTypes.string.isRequired,
+};
 
 export default CowSays;
